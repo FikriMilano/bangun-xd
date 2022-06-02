94760ee53e4c9ad1bf1c819637f29947e431dd8b
fikrimilano <fikrimilano1@gmail.com>
2022-06-02 07:28:10 +0700

    Print instance for easier logging

--- src/main/java/bangun3d/LimasApp.java
+++ src/main/java/bangun3d/LimasApp.java
@@ -7,6 +7,7 @@ public class LimasApp {
 
         Bangun2D kucut = new Lingkaran(6);
         Limas alasling = new Limas(kucut, 8);
+        System.out.println(alasling);
         System.out.println(alasling.getLuas());
         System.out.println(alasling.getVolume());
 
@@ -14,6 +15,7 @@ public class LimasApp {
 
         Persegi persegi = new Persegi(10);
         Limas alasper = new Limas(persegi, 12);
+        System.out.println(alasper);
         System.out.println(alasper.getLuas());
         System.out.println(alasper.getVolume());
 
@@ -21,6 +23,7 @@ public class LimasApp {
 
         PersegiPanjang pp = new PersegiPanjang(6, 5);
         Limas alaspp = new Limas(pp, 4);
+        System.out.println(alaspp);
         System.out.println(alaspp.getLuas());
         System.out.println(alaspp.getVolume());
 
@@ -28,6 +31,7 @@ public class LimasApp {
 
         Segitiga st = new SegitigaSamaKaki(10, 15);
         Limas alasst = new Limas(st, 8);
+        System.out.println(alasst);
         System.out.println(alasst.getLuas());
         System.out.println(alasst.getVolume());
 
